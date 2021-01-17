86a32bfaa631cbf376b20d11d80293b3e1c6eca3
Saran <romansaran446@gmail.com>
2021-01-17 14:20:48 +0530

    rendered  post api , fetching user api

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,14 +1,21 @@
 import jsonplaceholder from '../apis/jsonPlaceHoder';
 
-export const fetchPosts  =  () => 
- async  dispath =>{
+export const fetchPosts  =  () => async  dispath => {
     const response =  await jsonplaceholder.get('/posts');
 
     dispath({
         type:'FETCH_POSTS',
-        payload:response
-    })
+        payload:response.data,
+    });
 };
 
 
 
+export const fetchUser = (id) => async dispatch => {
+    const response = await jsonplaceholder.get(`/users/${id}`);
+
+    dispatch ({
+        type : 'FETCH_USER',
+        payload: response.data,
+    })
+}
--- blog/src/reducers/postReducers.js
+++ blog/src/reducers/postReducers.js
@@ -1,7 +1,11 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (state=[],action) => {
         switch (action.type) {  // if action.type = case 'fetch_posts
             case 'FETCH_POSTS':
                 return action.payload;
+
+            default:
+                return state;
 
         }
 };
