d51ab15783d2f63a8e8ffe709ce42cb4449d274c
Saran <romansaran446@gmail.com>
2021-01-17 10:30:23 +0530

    setup

---
+++ blog/src/Components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PostList from './PostList';
+
+const App = () => {
+    return (
+        <div className="ui container">
+            <h1><PostList/></h1>
+            </div>
+    );
+
+}
+
+export default App;
