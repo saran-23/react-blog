1b5d3c63f16a37d1e427884a8ce3a3d384df2861
Saran <romansaran446@gmail.com>
2021-01-17 13:24:47 +0530

    reducers- default(null,{},[]) no mutatble,

--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,5 +1,5 @@
 import {combineReducers} from 'redux';
-
+import postReducers from '../reducers/postReducers';
 export default combineReducers({
-    replaceMe: () => 'hi there'
+    posts: postReducers
 });
