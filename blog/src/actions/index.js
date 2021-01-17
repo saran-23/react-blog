aa546996b9164cd5bc8a6cd028b2d3879d4b935c
Saran <romansaran446@gmail.com>
2021-01-17 12:38:42 +0530

    middleware,thunk

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,11 +1,13 @@
 import jsonplaceholder from '../apis/jsonPlaceHoder';
 
-export const fetchPosts  = async () => {
-   const response = await jsonplaceholder.get('/posts');
-    return {
+export const fetchPosts  =  () => 
+ async  dispath =>{
+    const response =  await jsonplaceholder.get('/posts');
+
+    dispath({
         type:'FETCH_POSTS',
-        payload: response,
-    };
+        payload:response
+    })
 };
 
 
