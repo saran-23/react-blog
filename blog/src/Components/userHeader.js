f416e4a55df23e27aa16a421efef0eeb76f237cf
Saran <romansaran446@gmail.com>
2021-01-18 15:03:24 +0530

    fixed author name

--- blog/src/Components/userHeader.js
+++ blog/src/Components/userHeader.js
@@ -8,7 +8,7 @@ class UserHeader extends React.Component {
     }
     
     render() {
-        const user = this.props;
+        const  {user }= this.props;
         if (!user)  {
              return null;
         }
@@ -18,8 +18,8 @@ class UserHeader extends React.Component {
     }
 }
 
-const mapStateToProps = (state,ownProps)  => {
-    return {users: state.users.find(user => user.id === ownProps.userId) };
+const  mapStateToProps = (state,ownProps)  => {
+    return {user: state.users.find(user => user.id === ownProps.userId) };
 };
  
-export default connect(mapStateToProps,{fetchUser})(UserHeader);
+export default connect(mapStateToProps, {fetchUser})(UserHeader);
