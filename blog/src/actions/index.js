95e22a75cf11ef04589725d91341e9602edbd315
Saran <romansaran446@gmail.com>
2021-01-19 19:12:17 +0530

    no _.

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -10,13 +10,23 @@ export const  fetchPosts  =  () => async  dispatch => {
     });
 };
 
-export const fetchUser = id =>  dispatch =>  _fetchUser(id,dispatch);     //(id,dispatch);   defines that what u need  
-
-const  _fetchUser =  _.memoize(async (id,dispatch) =>{                             //private  function  _fetchuser
+export const fetchUser = id => async  dispatch =>  {
     const response = await jsonplaceholder.get(`/users/${id}`);
 
     dispatch ({
         type : 'FETCH_USER',
         payload: response.data,
     });
-});
+};
+
+
+// export const fetchUser = id =>  dispatch =>  _fetchUser(id,dispatch);     //(id,dispatch);   defines that what u need  
+
+// const  _fetchUser =  _.memoize(async (id,dispatch) =>{                             //private  function  _fetchuser
+//     const response = await jsonplaceholder.get(`/users/${id}`);
+
+//     dispatch ({
+//         type : 'FETCH_USER',
+//         payload: response.data,
+//     });
+// });
